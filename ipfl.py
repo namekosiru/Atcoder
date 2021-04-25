c562a328cdbc78b797ebdefe602ecbfec528750d
namekosiru <rinjyou0031@toki.waseda.jp>
2021-04-25 21:41:52 +0900

    4/25

--- ipfl.py
+++ ipfl.py
@@ -1,15 +1,38 @@
-N = int(input())
+n = int(input())
 s = list(input())
 q = int(input())
-lis = []
-for _ in range(q):
-    lis.append(list(map(int, input().split())))
 
-for li in lis:
-    if li[0] == 1:
-        tmp = s[li[1]-1]
-        s[li[1]-1] = s[li[2]-1]
-        s[li[2]-1] = tmp
-    elif li[0] == 2:
-        s = s[len(s)//2:] + s[:len(s)//2]
-print("".join(s))
+flip = 0
+
+for i in range(q):
+    t, a, b = map(int, input().split())
+    a -= 1
+    b -= 1
+
+    if t == 2:
+        flip += 1
+        # tmp = s[li[1]-1]
+        # s[li[1]-1] = s[li[2]-1]
+        # s[li[2]-1] = tmp
+    elif t == 1:
+        if flip % 2 == 0:
+            a, b = b, a
+        else:
+            if a >= n:
+                a -= n
+                b -= n
+            elif  b >= n and a < n:
+                a += n
+                b -= n
+            else:
+                a += n
+                b += n
+
+        s[a], s[b] = s[b], s[a]
+
+if flip % 2 == 0:
+    print(*s, sep="")
+else:
+    s = s[n:] + s[:n]
+    print(*s, sep="")
+
